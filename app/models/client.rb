ac1aa33eb0ebb6077a70159cd4e2d53ecf0a82bc
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2019-05-24 20:11:32 -0300

    has many associations

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,4 @@
 class Client < ApplicationRecord
+  has_many :clients
   validates :name, presence: :true, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   belongs_to :client
+  has_many :projects
   validates :first_name, :last_name, presence: true
 
   def name
