afdf1700c1024871a557ac372b58d3f8b7037179
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2019-05-26 00:06:10 -0300

    fixed params permitted

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -2,10 +2,13 @@
 class Api::ApplicationController < ActionController::API
   # config.web_console.whitelisted_ips = %w( 0.0.0.0/0 ::/0 )
   def current_user
-    require 'json'
-    new_params = JSON.parse(params.keys.first)
-    new_params = new_params.symbolize_keys[:user].symbolize_keys
-    return nil if new_params[:auth_token].blank?
-    @current_user ||= User.find_by_authentication_token(new_params[:auth_token])
+    return nil if user_logged_params[:auth_token].blank?
+    @current_user ||= User.find_by_authentication_token(user_logged_params[:auth_token])
   end
+
+  protected
+
+    def user_logged_params
+      params.require(:user).permit(:auth_token)
+    end
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,47 +1,45 @@
 class Api::UsersController < Api::ApplicationController
   def create
-    client = Client.find_by_name(sign_up_params[:client_name])
+    client = Client.find_by_name(user_params[:client_name])
     return head 404 if client.nil?
 
-    email, password, uuid = sign_up_params[:email], sign_up_params[:password], sign_up_params[:uuid]
+    email, password, uuid = user_params[:email], user_params[:password], user_params[:uuid]
     user = User.find_by_email(email)
     return head 401 if user
     
     user = client.users.build(
-      first_name: sign_up_params[:first_name],
-      last_name: sign_up_params[:last_name],
-      email: sign_up_params[:email],
-      password: sign_up_params[:password],
-      password_confirmation: sign_up_params[:password_confirmation],
+      first_name: user_params[:first_name],
+      last_name: user_params[:last_name],
+      email: user_params[:email],
+      password: user_params[:password],
+      password_confirmation: user_params[:password_confirmation],
     )
     unless user.save
       logger.info user.errors.full_messages
       return head 422
     end
     user.ensure_authentication_token!
-    Device.register(sign_up_params, user)
+    Device.register(user_params, user)
 
     render json: { auth_token: user.authentication_token }
   end
 
   def sign_in
-    email, password = sign_up_params[:email], sign_up_params[:password]
+    email, password = user_params[:email], user_params[:password]
     user = User.find_by_email(email)
     return head 404 if user.nil?
 
     return head 400 unless user.valid_password?(password)
 
     user.ensure_authentication_token!
-    Device.register(sign_up_params, user)
+    Device.register(user_params, user)
 
     render json: { auth_token: user.authentication_token }
   end
   
 
   protected
-    def sign_up_params
-      require 'json'
-      new_params = JSON.parse(params.keys.first)
-      new_params.symbolize_keys[:user].symbolize_keys
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :client_name, :password, :password_confirmation, :uuid, :gcm_reg_id, :version, :loading)
     end
 end
