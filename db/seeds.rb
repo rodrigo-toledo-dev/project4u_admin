c3af0eae73e2bd0c7e05605afc7a00cbbcf65631
Rodrigo Toledo <rodrigo.toledo.dev@gmail.com>
2019-06-11 18:53:14 -0300

    use right version of ruby in project, change password

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@ User.destroy_all
 Client.destroy_all
 Client.create!(name: 'Project4U')
 client = Client.first
-client.users.build(first_name: 'Rodrigo', last_name: 'Toledo', password: 'asdqwe123', password_confirmation: 'asdqwe123', email: 'rodrigo@rtoledo.inf.br')
-client.users.build(first_name: 'Denise', last_name: 'Almeida', password: 'asdqwe123', password_confirmation: 'asdqwe123', email: 'denise.almeida@agence.com.br')
+client.users.build(first_name: 'Rodrigo', last_name: 'Toledo', password: 'asdasdasd', password_confirmation: 'asdasdasd', email: 'rodrigo@rtoledo.inf.br')
+client.users.build(first_name: 'Denise', last_name: 'Almeida', password: 'asdasdasd', password_confirmation: 'asdasdasd', email: 'denise.almeida@agence.com.br')
 client.save!
